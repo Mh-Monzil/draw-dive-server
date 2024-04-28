cdc75eb42b6142922c74321818520c17980aac46
Mh-Monzil <monzil246@gmail.com>
2024-04-28 23:58:15 +0600

    delete method added

--- index.js
+++ index.js
@@ -58,6 +58,13 @@ async function run() {
         res.send(result);
     })
 
+
+    app.delete('/craft_items/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await itemCollection.deleteOne(query);
+      res.send(result);
+    })
     
 
     // Send a ping to confirm a successful connection
